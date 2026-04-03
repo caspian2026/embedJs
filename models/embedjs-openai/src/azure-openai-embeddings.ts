149d857397ac0a4aa87ddea0df5488c1a491b145
刘希楠 <yhda66@126.com>
2026-04-03 13:44:37 +0800

    fix: 修复Error invoking remote method 'knowledge-base:create' , 修复打包错误

--- models/embedjs-openai/src/azure-openai-embeddings.ts
+++ models/embedjs-openai/src/azure-openai-embeddings.ts
@@ -12,17 +12,18 @@ export class AzureOpenAiEmbeddings extends BaseEmbeddings {
         if (!this.configuration) this.configuration = {};
         if (!this.configuration.model) this.configuration.model = 'text-embedding-3-small';
 
-        if (!this.configuration.dimensions) {
-            if (this.configuration.model === 'text-embedding-3-small') {
-                this.configuration.dimensions = 1536;
-            } else if (this.configuration.model === 'text-embedding-3-large') {
-                this.configuration.dimensions = 3072;
-            } else if (this.configuration.model === 'text-embedding-ada-002') {
-                this.configuration.dimensions = 1536;
-            } else {
-                throw new Error('You need to pass in the optional dimensions parameter for this model');
-            }
-        }
+        // https://github.com/llm-tools/embedJs/issues/212
+        // if (!this.configuration.dimensions) {
+        //     if (this.configuration.model === 'text-embedding-3-small') {
+        //         this.configuration.dimensions = 1536;
+        //     } else if (this.configuration.model === 'text-embedding-3-large') {
+        //         this.configuration.dimensions = 3072;
+        //     } else if (this.configuration.model === 'text-embedding-ada-002') {
+        //         this.configuration.dimensions = 1536;
+        //     } else {
+        //         throw new Error('You need to pass in the optional dimensions parameter for this model');
+        //     }
+        // }
 
         this.model = new LangchainAzureOpenAiEmbeddings(this.configuration, this.options);
     }
